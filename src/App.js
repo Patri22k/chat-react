42f59551e3602a296c6ae863c0a396ef0ae44f7a
Patri22k <bajzik.patrik6@gmail.com>
2024-01-24 23:03:21 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
 
 function App() {
+  const [value, setValue] = useState(undefined);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value && value.length > 0) {
+      setList([...list, value]);
+      setValue(undefined);
+      document.getElementById('chat-border').scrollIntoView();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="text-bar">
+        <div className='chat'>
+          {list.map(val => (
+            <div className='inner-chat-wrapper'>
+              <div className='inner-chat'><p>{val}</p></div>
+            </div>
+          ))}
+          <div id="chat-border"></div>
+        </div>
+        <form className='inner-text-bar'
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <input 
+            type="text" 
+            className='input-text' 
+            onChange={(e) => setValue(e.target.value)}
+            value = {value ?? ""}
+          />
+          <button
+          type="submit">
+            <FontAwesomeIcon 
+              icon={faPaperPlane} 
+              className='send-icon'
+            />
+          </button>
+        </form>
+      </div>
+    </>
   );
 }
 
